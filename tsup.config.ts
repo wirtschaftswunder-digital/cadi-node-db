e3e48a9a05ecae7b42b3afd5a226c16220156b51
Leon <leon.mainusch@web.de>
2025-06-04 11:00:25 +0200

    FIX: tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['esm'],
-    dts: true,
+    dts: {
+        entry: ['src/index.ts'],
+        resolve: true,
+    },
     clean: true,
     sourcemap: true,
     bundle: true,
